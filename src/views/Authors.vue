b63eb5963554e1b84deee9f2907fc2d39f83ceb0
Loy Dockery <loydockery@gmail.com>
2021-05-31 14:10:42 -0700

    added AuthorList component

--- src/views/Authors.vue
+++ src/views/Authors.vue
@@ -2,14 +2,16 @@
   <div id="authors">
     <h1>AUTHORS</h1>
     <AuthorCreate />
+    <AuthorList />
   </div>
 </template>
 
 <script>
-import AuthorCreate from '../components/AuthorCreate.vue'
+import AuthorCreate from '@/components/AuthorCreate.vue'
+import AuthorList from '@/components/AuthorList.vue'
 
 export default {
   name: 'Authors',
-  components: { AuthorCreate },
+  components: { AuthorCreate, AuthorList },
 }
 </script>
