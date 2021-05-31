c71ab06f8fc5518f985facd7d26fe0a8479a73a4
Loy Dockery <loydockery@gmail.com>
2021-05-30 20:07:51 -0700

    fixed prettier/prettier issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,19 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: 'babel-eslint'
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // "prettier/prettier": ["error", {"singleQuote": true, "parser": "flow"}]
+    'prettier/prettier': [
+      'error',
+      { singleQuote: true, endOfLine: 'auto', semi: false }
+    ]
+  }
+}
